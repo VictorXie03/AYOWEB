5cc780c92686d2e2df4620804e003f9f76c2ef44
VictorXie03 <victorxie75@gmail.com>
2024-11-21 18:16:07 -0500

    added static menu link

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import menuLeft from '../../images/menu-left.svg'
-import menu from '../../images/ayofullmenu.pdf'
 import MenuCarousel from './MenuCarousel'
 import './Menu.css'
 
@@ -14,7 +13,7 @@ const Menu = () => {
           <div className="Menu-info">
             <h1 className="Menu-title">Menu</h1>
             <p className="Menu-paragraph">Whether you're seeking the familiar comfort of classic Chinese dishes or craving the excitement of bold new flavors, our menu has something to delight every palate.</p>
-            <a href={menu} target="_blank" className="button">Full Menu</a>
+            <a href={"https://drive.google.com/file/d/1tdTrGd1V2_tn1vqfl2JjpuOLFgQJ25dG/view?usp=sharing"} target="_blank" className="button">Full Menu</a>
           </div>
         </div>
       </div>
--- src/components/TopCarousel/TopCarousel.js
+++ src/components/TopCarousel/TopCarousel.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './TopCarousel.css'
 import topImage from '../../images/beef-vegetables-sesame-seeds-black-bowl-black-slate-background.jpg'
-import menu from '../../images/ayofullmenu.pdf'
 
 
 
@@ -14,7 +13,7 @@ const TopCarousel = () => {
             <img
                 className='Top-image' id='home' src={topImage}>
             </img>
-            <a href={menu} target="_blank" className="top-button">Full Menu</a>
+            <a href={"https://drive.google.com/file/d/1tdTrGd1V2_tn1vqfl2JjpuOLFgQJ25dG/view?usp=sharing"} target="_blank" className="top-button">Full Menu</a>
             <a className="top-call-button" href="tel://+17054245555">Call Now</a>
             <div className="bottomright">Designed by Freepik</div>
         </div>
