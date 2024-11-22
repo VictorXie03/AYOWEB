c3bbdb80678057756d4cee8457a19ac4693d9978
VictorXie03 <victorxie75@gmail.com>
2024-11-21 19:03:57 -0500

    updated menu link

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import menuLeft from '../../images/menu-left.svg'
 import MenuCarousel from './MenuCarousel'
 import './Menu.css'
+import menu from '../../images/menu.pdf'
 
 
 const Menu = () => {
@@ -13,7 +14,7 @@ const Menu = () => {
           <div className="Menu-info">
             <h1 className="Menu-title">Menu</h1>
             <p className="Menu-paragraph">Whether you're seeking the familiar comfort of classic Chinese dishes or craving the excitement of bold new flavors, our menu has something to delight every palate.</p>
-            <a href={"https://drive.google.com/file/d/1tdTrGd1V2_tn1vqfl2JjpuOLFgQJ25dG/view?usp=sharing"} target="_blank" className="button">Full Menu</a>
+            <a href={menu} target="_blank" className="button">Full Menu</a>
           </div>
         </div>
       </div>
